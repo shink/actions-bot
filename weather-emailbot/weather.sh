25c31d30b27bf572343777aae51ea593e2359429
profoundly <1224117064@qq.com>
2020-01-30 18:25:12 +0800

    merge

---
+++ weather-emailbot/weather.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -eux
+
+CITY=Xian
+LANGUAGE="zh-CN"
+UNIT=m
+UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+
+curl \
+  -H "Accept-Language: $LANGUAGE" \
+  -H "User-Agent: $UA" \
+  -o result.html \
+  wttr.in/$CITY?format=3\&$UNIT
