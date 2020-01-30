e2a855fedfb23c293a3fc09bb1bce0f101f9fec0
profoundly <1224117064@qq.com>
2020-01-30 11:53:21 +0800

    update

--- main.sh
+++ main.sh
@@ -2,15 +2,6 @@
 
 set -eux
 
-filePath="/home/result/config.json"
-if [ ! -f "$filePath" ];then
-touch $filePath
-echo "{}" > $filePath
-echo "文件创建完成"
-else
-echo "文件已经存在"
-fi
-
 CSDN_ID="qq_38105251"
 mail_host="smtp.163.com"
 mail_port=465
@@ -18,5 +9,16 @@ mail_user="shenkebug@163.com"
 mail_password="wuTAwuai10190013"
 sender="shenkebug@163.com"
 receiver="shenkebug@qq.com"
+filePath="/home/result/config.json"
+
+# if [ ! -f "$filePath" ];then
+# touch $filePath
+# echo "{}" > $filePath
+# echo "文件创建完成"
+# else
+# echo "文件已经存在"
+# fi
+
+pwd
 
 python spider.py $CSDN_ID $mail_host $mail_port $mail_user $mail_password $sender $receiver $filePath
