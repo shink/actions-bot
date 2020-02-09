c20afe4d9a50d44a33bed4ae6a9093df94dc232f
sculpta <shenkebug@qq.com>
2020-02-09 16:03:47 +0800

    fix bug

--- csdn-emailbot/spider.py
+++ csdn-emailbot/spider.py
@@ -4,103 +4,97 @@
 import sys
 import requests
 from bs4 import BeautifulSoup
-import time
-import numpy as np
+import json
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
 
 
-# 访问网页
-def access_page(url):
-    user_agent_list = [
-        'Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24',
-        'Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6',
-        'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3',
-        'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20',
-        'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)',
-        'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1',
-        'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6',
-        'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3',
-        'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
-        'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24',
-        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)',
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5',
-        'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1',
-        'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-        'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)',
-        'Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11',
-        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; 360SE)',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3',
-        'Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10',
-        'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER',
-        'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52',
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3',
-        'Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5',
-        'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)',
-        'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.3 Mobile/14E277 Safari/603.1.30',
-        'Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)',
-        'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3',
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0',
-        'Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)',
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)',
-        'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1',
-        'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6',
-        'Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5']
-
-    user_agent = np.random.choice(user_agent_list, 1)[0]
-    headers = {"user-agent": user_agent}
+# 爬取访问量、排名等信息
+def getResult(CSDN_ID):
+    url = "https://blog.csdn.net/" + CSDN_ID
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+        "accept-language": "zh-CN,zh;q=0.9"
+    }
 
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        return response.text
-    else:
-        return None
+        result = {
+            "nick_name": "",
+            "blog_title": "",
+            "profile": {}
+        }
+
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        # 爬取asideProfile信息
+        nick_name = soup.find("a", id="uid").get("title")
+        blog_title = soup.find("a", attrs={"href": url}).get_text().strip()
+        result["nick_name"] = nick_name
+        result["blog_title"] = blog_title
+
+        profile = {}
+        profile_data = []
+        data_info = soup.find("div", attrs={"class": "data-info d-flex item-tiling"})
+        data = data_info.find_all("dl", attrs={"class": "text-center"})
+        for num in data:
+            profile_data.append(num.attrs["title"])
+
+        grade_info = soup.find("div", attrs={"class": "grade-box clearfix"}).contents
+        point = grade_info[5].find("dd").attrs["title"]
+        week_rank = grade_info[3].attrs["title"]
+        total_rank = grade_info[7].attrs["title"]
+
+        profile["original"] = profile_data[0]
+        profile["fans"] = profile_data[1]
+        profile["like"] = profile_data[2]
+        profile["comment"] = profile_data[3]
+        profile["read"] = profile_data[4]
+        profile["point"] = point
+        profile["week_rank"] = week_rank
+        profile["total_rank"] = total_rank
+        result["profile"] = profile
+
+        return result
 
 
-# 爬取账户信息
-def access_profile(text):
-    soup = BeautifulSoup(text, "html.parser")
-    nick_name = soup.find("a", id="uid").get("title")
-    read = soup.find_all("dl", attrs={"class": "text-center"})[-1].get("title")
-    return nick_name, read
+# 生成信息
+def formatMessage(result):
+    message = ""
+    call = "亲爱的 " + result["nick_name"] + "，您的 CSDN profile 到啦\n\n"
+    profile = result["profile"]
+    original = "原创文章数目： " + profile["original"] + "\n"
+    fans = "粉丝： " + profile["fans"] + "\n"
+    like = "获赞： " + profile["like"] + "\n"
+    comment = "评论： " + profile["comment"] + "\n"
+    read = "访问量： " + profile["read"] + "\n"
+    point = "积分： " + profile["point"] + "\n"
+    week_rank = "周排名： " + profile["week_rank"] + "\n"
+    total_rank = "总排名： " + profile["total_rank"]
+
+    message += call + original + fans + like + comment + read + point + week_rank + total_rank
+    return message
 
 
-# 爬取该页面下的文章
-def access_article(text):
-    global article_sum
+# 发送邮件
+def sendEmail(content):
+    message = MIMEText(content, 'plain', 'utf-8')
+    message['From'] = "GitHub Actions<" + sender + ">"
+    message['To'] = "<" + receiver + ">"
 
-    soup = BeautifulSoup(text, "html.parser")
+    subject = "CSDN Report"
+    message['Subject'] = Header(subject, 'utf-8')
 
-    # 爬取每条文章的信息
-    articles = soup.find_all(["strong", "h4"])
-    if (articles == []):
-        return 0
-    else:
-        # 随机爬取文章
-        length = len(articles)
-        index_array = np.random.permutation(np.arange(length))
-        for index in index_array:
-            article = articles[index]
-            article_title = article.a.get_text().strip()
-            article_url = article.a.get("href")
+    try:
+        smtpObj = smtplib.SMTP_SSL(mail_host, mail_port)
+        smtpObj.login(mail_user, mail_password)
+        smtpObj.sendmail(sender, receiver, message.as_string())
+        print("邮件发送成功")
 
-            # 随机生成一个间隔时间（范围：15~25分钟）
-            interval = np.random.randint(15 * 60, 25 * 60, 1)
-            access_page(article_url)
-            article_sum += 1
-            time.sleep(interval)
-
-        return 1
+    except smtplib.SMTPException:
+        print("Error: 无法发送邮件")
 
 
 # 保存email内容
@@ -113,33 +107,7 @@ if __name__ == "__main__":
 
     CSDN_ID = sys.argv[1]
 
+    res = getResult(CSDN_ID)
+    message = formatMessage(res)
     email_path = "email.txt"
-    page_num = 1
-    url_prefix = "https://blog.csdn.net/" + CSDN_ID + "/article/list/"
-    url = url_prefix + str(page_num)
-
-    article_sum = 0
-
-    try:
-        while (1):
-            print("第" + str(page_num) + "页：")
-            if (access_article(access_page(url))):
-                page_num += 1
-                url = url_prefix + str(page_num)
-            else:
-                print("目前在第" + str(page_num) + "页，该页没有内容，结束")
-                time.sleep(20)
-                break
-
-        # 任务完成后，获取访问量
-        url = url_prefix + str(1)
-        nick_name, read = access_profile(access_page(url))
-        call = "亲爱的 " + nick_name + "，您的 csdn spider 完成任务啦\n"
-        message = call + "总共帮您访问了 " + str(article_sum) + " 篇文章，目前博客的访问量是：" + str(read)
-
-        saveEmail(email_path, message)
-
-    except TypeError:
-        print("该url返回为空")
-    except Exception as e:
-        print("error: " + str(e))
+    saveEmail(email_path, message)
